580da861b45f28bead9151afa8cd4a1c326ec530
Jamilly Vieira Da Silva <159734723+Millydev09@users.noreply.github.com>
2024-09-23 15:04:43 -0300

    Add files via upload

---
+++ celcius para farenheit.cpp
@@ -0,0 +1,17 @@
+//EXSEQU04A.CPP
+
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+   float c, f;
+
+   cout << "temperatura em graus celcius:";
+   cin >> c;
+
+   f=c*9/5+32;
+   cout << "a temperatura em fareheit é" << f;
+  return 0;
+}
